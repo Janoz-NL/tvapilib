beef774cfdbd38056dd79b6b13d2789c60bdbf92
gijs.de.vries <gijs.de.vries@d258bc06-591b-b093-c998-2941dcb50992>
2011-10-29 07:37:14 +0000

    id is not seriesid

--- src/main/java/com/janoz/tvapilib/thetvdb/impl/ShowParser.java
+++ src/main/java/com/janoz/tvapilib/thetvdb/impl/ShowParser.java
@@ -26,8 +26,10 @@ public class ShowParser<Sh extends IShow<Sh,Se,Ep>, Se extends ISeason<Sh,Se,Ep>
 	
 	public void handleContent(List<String> stack, String content){
 		if (stack.size()==1) {
-			if ("seriesid".equals(stack.get(0))) {
+			if ("id".equals(stack.get(0))) {
 				show.setTheTvDbId(Integer.valueOf(content));
+//			} else if ("seriesid".equals(stack.get(0))) {
+//				show.setTheTvDbId(Integer.valueOf(content));
 			} else if ("seriesname".equals(stack.get(0))) {
 				show.setTitle(content);
 			} else if ("overview".equals(stack.get(0))) {
