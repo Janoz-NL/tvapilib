ca4708045d9e7acf4acbeea86f935b4fe96abeb3
gijs.de.vries <gijs.de.vries@d258bc06-591b-b093-c998-2941dcb50992>
2011-11-21 19:55:12 +0000

    Initial import of xbmc nfo implementation

--- src/main/java/com/janoz/tvapilib/xbmcnfo/impl/XbmcNfoImpl.java
+++ src/main/java/com/janoz/tvapilib/xbmcnfo/impl/XbmcNfoImpl.java
@@ -12,7 +12,12 @@ package com.janoz.tvapilib.xbmcnfo.impl;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import com.janoz.tvapilib.model.IEpisode;
 import com.janoz.tvapilib.model.ISeason;
@@ -39,20 +44,76 @@ public class XbmcNfoImpl<Sh extends IShow<Sh,Se,Ep>, Se extends ISeason<Sh,Se,Ep
 
 	@Override
     public void saveShow(Sh show, File nfoFile) throws IOException {
-	    // TODO Auto-generated method stub
-		throw new UnsupportedOperationException();
+		nfoFile.createNewFile();
+		OutputStreamWriter osw = createUtf8FileWriter(nfoFile);
+		osw.write("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<tvshow>\n");
+		writeTag(osw,"id",show.getTheTvDbId());
+		writeTag(osw,"tvdbid",show.getTheTvDbId());
+		writeTag(osw,"title",show.getTitle());
+		writeTag(osw,"plot",show.getDescription());
+		writeTag(osw,"studio",show.getNetwork());
+		writeTag(osw,"mpaa",show.getContentRating());
+		osw.write("</tvshow>");
+		osw.close();
+    }
+	
+	@Override
+    public void saveEpisode(Ep episode, File nfoFile) throws IOException {
+		nfoFile.createNewFile();
+		OutputStreamWriter osw = createUtf8FileWriter(nfoFile);
+/*
+ <episodedetails>
+        <rating>10.00</rating>
+        <thumb>http://thetvdb.com/banners/episodes/164981/2528821.jpg</thumb>
+        <credits>Writer</credits>
+        <director>Mr. Vision</director>
+        
+        <studio>Production studio or channel</studio>
+        <mpaa>MPAA certification</mpaa>
+ */
+		osw.write("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<episodedetails>\n");
+		writeTag(osw,"title",episode.getTitle());
+		writeTag(osw,"season",episode.getSeason().getSeason());
+		writeTag(osw,"episode",episode.getEpisode());
+		writeTag(osw,"plot",episode.getDescription());
+		writeTag(osw,"aired",episode.getAired());
+		osw.write("</episodedetails>");
+		osw.close();
     }
 
+
+	private OutputStreamWriter createUtf8FileWriter(File file) throws IOException {
+		return new OutputStreamWriter(new FileOutputStream(file), "UTF8");
+	}
+	
+	private String encode(String input) {
+		return input.replaceAll("&", "&amp;").replaceAll("<", "&lt;").replaceAll("<", "&gt;");
+	}
+	
+	private void writeTag(Writer w, String tag, Date value) throws IOException {
+		if (value == null) return;
+		writeTag(w, tag,new SimpleDateFormat("yyyy-MM-dd").format(value));
+	}
+
+	private void writeTag(Writer w, String tag, Integer value) throws IOException {
+		if (value == null) return;
+		writeTag(w, tag,value.toString());
+	}
+	
+	private void writeTag(Writer w, String tag, String value) throws IOException {
+		if (value == null) return;
+		w.write("\t<");
+		w.write(tag);
+		w.write(">");
+		w.write(encode(value.toString()));
+		w.write("</");
+		w.write(tag);
+		w.write(">\n");
+	}
+	
 	@Override
 	public Ep loadEpisode(File nfoFile) throws IOException {
 		// TODO Auto-generated method stub
 		throw new UnsupportedOperationException();
 	}
-
-	@Override
-    public void saveEpisode(Ep episode, File nfoFile) throws IOException {
-	    // TODO Auto-generated method stub
-		throw new UnsupportedOperationException();
-    }
-
 }
